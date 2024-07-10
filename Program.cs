1809e5114be1e75d0e2c82cb27c2823fdae3dcf6
ryuselmi <yuselmirodriguez24@gmail.com>
2024-07-10 10:04:46 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace week_02_EX_6_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double count = 0.0;
+            while (count != 10.0)
+            {
+                count = count + 1.0;
+                Console.Write(count / 10);
+                Console.ReadLine();
+            }
+        }
+    }
+}
+// It didn't stop because 'count' never becomes exactly '0.1'.
+// Since count is never exactly '1.0', the loop keeps running indefinitely.
